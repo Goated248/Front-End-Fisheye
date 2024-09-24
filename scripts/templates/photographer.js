886317fea209870e3052e2643124c1e2b7cf42e9
Ben Z <BenZ8248@outlook.com>
2024-09-24 16:30:05 +0200

    ajout de fetch sur getPhotographers modif du template d'affichage et du css

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,40 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait,city,country,tagline,price } = data
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait}`
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+
+        const article = document.createElement( 'article' )
+
+        const img = document.createElement( 'img' )
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        img.setAttribute("alt", `${name}`)
+
+        const h2 = document.createElement( 'h2' )
+        h2.textContent = name
+
+        const location = document.createElement( 'h3' )
+        location.textContent = `${city}, ${country}`
+        location.classList.add('location')
+
+        const taglineEl = document.createElement('p1')
+        taglineEl.textContent = tagline
+        taglineEl.classList.add('tagline')
+
+        const priceEl = document.createElement('p2')
+        priceEl.textContent = `${price}€/jour`
+        priceEl.classList.add('price') 
+
+
+
+        article.appendChild(img)
+        article.appendChild(h2)
+        article.appendChild(location)
+        article.appendChild(taglineEl)
+        article.appendChild(priceEl)
+
+        return (article)
     }
     return { name, picture, getUserCardDOM }
 }
